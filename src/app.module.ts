87ee4a5d09278a0aa8e386b7493f4f5da14d2bcd
didi3762 <7127821@gmail.com>
2020-12-10 15:17:38 +0200

    change path

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'angular','dist','gameproj'),
+      rootPath: join(__dirname,'..','..','angular','dist','gameproj'),
     }),
     UsersModule,
      DatabaseModule, 
@@ -52,7 +52,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 })
 export class AppModule {
   constructor(){
-      console.log(join(__dirname,'angular','dist','gameproj'));
+      console.log(join(__dirname,'..','..','angular','dist','gameproj'));
       
   }
 }
